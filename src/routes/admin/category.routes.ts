0e33e39cdebbb0057ec3868955b14b12a8cd2e1e
safin-sys <mdsafinofficial@gmail.com>
2025-05-31 15:05:40 +0600

    fix: add missing get_categories route to category management

--- src/routes/admin/category.routes.ts
+++ src/routes/admin/category.routes.ts
@@ -1,6 +1,7 @@
 import {
   add_category,
   delete_category,
+  get_categories,
   update_category,
 } from "@controllers/category";
 import { Router } from "express";
@@ -12,6 +13,7 @@ const upload = multer({
   dest: "uploads/",
 });
 
+router.get("/", get_categories);
 router.post("/add", upload.single("icon"), add_category);
 router.patch("/update", upload.single("icon"), update_category);
 router.delete("/delete", upload.single("icon"), delete_category);
